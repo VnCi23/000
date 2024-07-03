7621ca3ff8abe370b1b2039230ec1e7b5db533b2
Your Name <vincegaurino@gmail.com>
2024-07-04 04:18:16 +0800

    2

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -49,7 +49,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                   py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
-                  <p>created with &hearts; by <a href={'https://github.com/VnCi23'}>VnCI</a></p>
+                  <p>created by: <a href={'https://github.com/VnCi23'}>VnCI</a></p>
                   <p>&copy; 2025</p>
                </Box>
             </Grid>
