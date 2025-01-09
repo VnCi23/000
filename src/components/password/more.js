fbc061d04088b0bda0c7eeb2d11053b50a44af5c
vincegaurino <vincegaurino@gmail.com>
2025-01-10 00:45:19 +0800

    refactor More component layout; enhance responsiveness and visual structure

--- src/components/password/more.js
+++ src/components/password/more.js
@@ -1,15 +1,71 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography, Container, Grid, Paper } from '@mui/material';
 
 export default function More() {
     return (
-        <Box className="container" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh', margin: '3rem', overflow: 'hidden', transform: 'skew(5deg)' }}>
-            {['Plotting', 'Angry', 'Curious', 'Prowling', 'Sleepy'].map((title, index) => (
-                <Box key={index} className="card" sx={{ flex: 1, transition: 'all 1s ease-in-out', height: '75vmin', position: 'relative', marginRight: index !== 4 ? '1em' : 0, '&:hover': { flexGrow: 10, '& img': { filter: 'grayscale(0)' }, '& .card__head': { textAlign: 'center', top: 'calc(100% - 2em)', color: 'white', fontSize: '2em', transform: 'rotate(0deg) skew(-5deg)' } } }}>
-                    <img src={`https://upload.wikimedia.org/wikipedia/commons/${index === 0 ? 'd/d6/Cat_plotting_something_evil%21.jpg' : index === 1 ? 'thumb/d/d8/False_alarm_-a.jpg/1280px-False_alarm_-a.jpg' : index === 2 ? 'thumb/8/83/Neugierige-Katze.JPG/1280px-Neugierige-Katze.JPG' : index === 3 ? 'thumb/7/75/Al_acecho_%289272124788%29.jpg/1280px-Al_acecho_%289272124788%29.jpg' : 'thumb/d/de/Mimi%26Tigsi.jpg/1280px-Mimi%26Tigsi.jpg'}`} alt={title} style={{ width: '100%', height: '100%', objectFit: 'cover', transition: 'all 1s ease-in-out' }} />
-                    <Box className="card__head" sx={{ color: 'black', background: 'rgba(0, 110, 255, 0.72)', padding: '0.5em', transform: 'rotate(-90deg)', transformOrigin: '0% 0%', transition: 'all 0.5s ease-in-out', minWidth: '100%', textAlign: 'center', position: 'absolute', bottom: 0, left: 0, fontSize: '1em', whiteSpace: 'nowrap' }}>{title}</Box>
-                </Box>
-            ))}
+        <Box component="section" sx={{ width: '100%'}}>
+            <Container sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', py: { md: 4, xs: 10 } }}>
+                <Grid container spacing={2} sx={{ width: { xl: '80%', sm: '85%', xs: '90%' }, mx: 'auto', mt: 4, flexDirection: { md: 'row', xs: 'column' }, justifyContent: 'center', alignItems: { lg: 'stretch', md: 'center' } }}>
+                    <Grid item md={6} xs={12}>
+                        <img style={{ width: '100%', height: 'auto', borderRadius: '4px 4px 0 0', objectFit: 'cover' }} src={require('../../img/m3.png')} alt="billboard" />
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <Paper sx={{ p: { md: 4, xs: 2 }, bgcolor: 'grey.100', color: 'text.secondary', borderRadius: 1, height: '100%' }}>
+                            <Typography variant="h3" sx={{ fontSize: { md: '1.5rem', xs: '1.5rem' }, fontWeight: 'fontWeightBold', color: 'text.primary' }}>
+                                Lorem ipsum dolor sit amet consectetur
+                            </Typography>
+                            <Typography variant="h3" fontSize={'1rem'} sx={{ mt: 4 }}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam veritatis, magni doloremque pariatur quos fugit ipsa id voluptatibus deleniti officiis cum ratione eligendi sed necessitatibus aliquam error laborum delectus quaerat. Delectus hic error eligendi sed repellat natus fuga nobis tempora possimus ullam!
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ width: { xl: '80%', sm: '85%', xs: '90%' }, mx: 'auto', mt: 6, flexDirection: { md: 'row', xs: 'column-reverse' }, justifyContent: 'center', alignItems: { lg: 'stretch', md: 'center' } }}>
+                    <Grid item md={6} xs={12}>
+                        <Paper sx={{ p: { md: 4, xs: 2 }, bgcolor: 'grey.100', color: 'text.secondary', borderRadius: 1, height: '100%' }}>
+                        <Typography variant="h3" sx={{ fontSize: { md: '1.5rem', xs: '1.5rem' }, fontWeight: 'fontWeightBold', color: 'text.primary' }}>
+                        Lorem ipsum dolor sit amet consectetur
+                            </Typography>
+                            <Typography variant="h3" fontSize={'1rem'} sx={{ mt: 4 }}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam veritatis, magni doloremque pariatur quos fugit ipsa id voluptatibus deleniti officiis cum ratione eligendi sed necessitatibus aliquam error laborum delectus quaerat. Delectus hic error eligendi sed repellat natus fuga nobis tempora possimus ullam!
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <img style={{ width: '100%', height: 'auto', borderRadius: '4px 4px 0 0', objectFit: 'cover' }} src={require('../../img/m2.png')} alt="billboard" />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ width: { xl: '80%', sm: '85%', xs: '90%' }, mx: 'auto', mt: 4, flexDirection: { md: 'row', xs: 'column' }, justifyContent: 'center', alignItems: { lg: 'stretch', md: 'center' } }}>
+                    <Grid item md={6} xs={12}>
+                        <img style={{ width: '100%', height: 'auto', borderRadius: '4px 4px 0 0', objectFit: 'cover' }} src={require('../../img/m1.png')} alt="billboard" />
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <Paper sx={{ p: { md: 4, xs: 2 }, bgcolor: 'grey.100', color: 'text.secondary', borderRadius: 1, height: '100%' }}>
+                        <Typography variant="h3" sx={{ fontSize: { md: '1.5rem', xs: '1.5rem' }, fontWeight: 'fontWeightBold', color: 'text.primary' }}>
+                        Lorem ipsum dolor sit amet consectetur
+                            </Typography>
+                            <Typography variant="h3" fontSize={'1rem'} sx={{ mt: 4 }}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam veritatis, magni doloremque pariatur quos fugit ipsa id voluptatibus deleniti officiis cum ratione eligendi sed necessitatibus aliquam error laborum delectus quaerat. Delectus hic error eligendi sed repellat natus fuga nobis tempora possimus ullam!
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ width: { xl: '80%', sm: '85%', xs: '90%' }, mx: 'auto', mt: 6, flexDirection: { md: 'row', xs: 'column-reverse' }, justifyContent: 'center', alignItems: { lg: 'stretch', md: 'center' } }}>
+                    <Grid item md={6} xs={12}>
+                        <Paper sx={{ p: { md: 4, xs: 2 }, bgcolor: 'grey.100', color: 'text.secondary', borderRadius: 1, height: '100%' }}>
+                        <Typography variant="h3" sx={{ fontSize: { md: '1.5rem', xs: '1.5rem' }, fontWeight: 'fontWeightBold', color: 'text.primary' }}>
+                        Lorem ipsum dolor sit amet consectetur
+                            </Typography>
+                            <Typography variant="h3" fontSize={'1rem'} sx={{ mt: 4 }}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam veritatis, magni doloremque pariatur quos fugit ipsa id voluptatibus deleniti officiis cum ratione eligendi sed necessitatibus aliquam error laborum delectus quaerat. Delectus hic error eligendi sed repellat natus fuga nobis tempora possimus ullam!
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <img style={{ width: '100%', height: 'auto', borderRadius: '4px 4px 0 0', objectFit: 'cover' }} src={require('../../img/m2.png')} alt="billboard" />
+                    </Grid>
+                </Grid>
+            </Container>
         </Box>
     );
 }
